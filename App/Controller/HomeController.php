4125db339c20ffacfd345533ff70bc2671562b40
StrahinjaGajic <strahinja.gajic.public@gmail.com>
2018-09-29 19:55:00 +0200

    Third

--- App/Controller/HomeController.php
+++ App/Controller/HomeController.php
@@ -13,38 +13,18 @@ class HomeController {
     public function post() {
 
             //Example for validation
-            $full_name = Validator::validateString($_POST['full_name']);
+            $first_name = Validator::validateString($_POST['first_name']);
             
-            $email = Validator::validateEmail($_POST['email']);
-            
-            $phone_num = Validator::validatePhoneNum($_POST['phone_num']);
-
-            $insurance_type = Validator::validateInsuranceType($_POST['insurance_type']);
-            
-            $start_date = Validator::validateDate($_POST['start_date']);
-
-            $end_date = Validator::validateDate($_POST['end_date']);
+            $second_name = Validator::validateString($_POST['secodn_name']);
 
             //Example save to database
             $propert = new User();
-            $property->insert();
+            $property->insert([
+                'first_name' =>$full_name,
+                'second_name' => $second_name]
+            );
 
             return Helper::sendSessionNotification('success','Success!');
-    }
-
-    public function listAll() {
-
-        $insurance = new Insurance();
-        $all_users = $insurance->getAll('user_insurance');
-        
-        Helper::view('list',$all_users);
-    }
-
-    public function listAllGroups() {
-        $insurance = new Insurance();
-        $all_group_insured = $insurance->getAllGroupInsured('user_group_insurance','user_insurance');
-
-        Helper::view('groupList',$all_group_insured);
     }
 }
 ?>
